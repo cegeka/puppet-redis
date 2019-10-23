cegeka/puppet-redis

commit 228a5376f75044c7d98419f077e99486b2bb217a

Complete the transition to puppet-strings

manifests/sentinel.pp
@@ -1,205 +1,140 @@
-# = Class: redis::sentinel
+# @summary Install redis-sentinel
 #
-# This class installs redis-sentinel
-#
-# == Parameters:
-#
-#
-# [*auth_pass*]
+# @param auth_pass
 #   The password to use to authenticate with the master and slaves.
 #
-#   Default: undef
-#
-# [*config_file*]
+# @param config_file
 #   The location and name of the sentinel config file.
 #
-#   Default for deb: /etc/redis/redis-sentinel.conf
-#   Default for rpm: /etc/redis-sentinel.conf
-#
-# [*config_file_orig*]
+# @param config_file_orig
 #   The location and name of a config file that provides the source
 #   of the sentinel config file. Two different files are needed
 #   because sentinel itself writes to its own config file and we do
 #   not want override that when puppet is run unless there are
 #   changes from the manifests.
 #
-#   Default for deb: /etc/redis/redis-sentinel.conf.puppet
-#   Default for rpm: /etc/redis-sentinel.conf.puppet
-#
-# [*config_file_mode*]
+# @param config_file_mode
 #   Permissions of config file.
 #
-#   Default: 0644
-#
-# [*conf_template*]
+# @param conf_template
 #   Define which template to use.
 #
-#   Default: redis/redis-sentinel.conf.erb
-#
-# [*daemonize*]
+# @param daemonize
 #   Have Redis sentinel run as a daemon.
 #
-#   Default: true
-#
-# [*down_after*]
+# @param down_after
 #   Number of milliseconds the master (or any attached slave or sentinel)
 #   should be unreachable (as in, not acceptable reply to PING, continuously,
 #   for the specified period) in order to consider it in S_DOWN state.
 #
-#   Default: 30000
-#
-# [*failover_timeout*]
+# @param failover_timeout
 #   Specify the failover timeout in milliseconds.
 #
-#   Default: 180000
-#
-# [*init_script*]
+# @param init_script
 #   Specifiy the init script that will be created for sentinel.
 #
-#   Default: undef on rpm, /etc/init.d/redis-sentinel on apt.
-#
-# [*log_file*]
+# @param log_file
 #   Specify where to write log entries.
 #
-#   Default: /var/log/redis/redis.log
-#
-# [*log_level*]
+# @param log_level
 #   Specify how much we should log.
 #
-#   Default: notice
-#
-# [*master_name*]
+# @param master_name
 #   Specify the name of the master redis server.
 #   The valid charset is A-z 0-9 and the three characters ".-_".
 #
-#   Default: mymaster
-#
-# [*redis_host*]
+# @param redis_host
 #   Specify the bound host of the master redis server.
 #
-#   Default: 127.0.0.1
-#
-# [*redis_port*]
+# @param redis_port
 #   Specify the port of the master redis server.
 #
-#   Default: 6379
-#
-# [*package_name*]
+# @param package_name
 #   The name of the package that installs sentinel.
 #
-#   Default: 'redis-server' on apt, 'redis' on rpm
-#
-# [*package_ensure*]
+# @param package_ensure
 #   Do we ensure this package.
 #
-#   Default: 'present'
-#
-# [*parallel_sync*]
+# @param parallel_sync
 #   How many slaves can be reconfigured at the same time to use a
 #   new master after a failover.
 #
-#   Default: 1
-#
-# [*pid_file*]
+# @param pid_file
 #   If sentinel is daemonized it will write its pid at this location.
 #
-#   Default: /var/run/redis/redis-sentinel.pid
-#
-# [*quorum*]
+# @param quorum
 #   Number of sentinels that must agree that a master is down to
 #   signal sdown state.
 #
-#   Default: 2
-#
-# [*sentinel_bind*]
+# @param sentinel_bind
 #   Allow optional sentinel server ip binding.  Can help overcome
 #   issues arising from protect-mode added Redis 3.2
 #
-#   Default: undef
-#
-# [*sentinel_port*]
+# @param sentinel_port
 #   The port of sentinel server.
 #
-#   Default: 26379
-#
-# [*service_group*]
+# @param service_group
 #   The group of the config file.
 #
-#   Default: redis
-#
-# [*service_name*]
+# @param service_name
 #   The name of the service (for puppet to manage).
 #
-#   Default: redis-sentinel
-#
-# [*service_owner*]
+# @param service_owner
 #   The owner of the config file.
 #
-#   Default: redis
-#
-# [*service_enable*]
+# @param service_enable
 #   Enable the service at boot time.
 #
-#   Default: true
-#
-# [*working_dir*]
+# @param working_dir
 #   The directory into which sentinel will change to avoid mount
 #   conflicts.
 #
-#   Default: /tmp
-#
-# [*notification_script*]
+# @param notification_script
 #   Path to the notification script
 #
-#   Default: undef
-#
-# [*client_reconfig_script*]
+# @param client_reconfig_script
 #   Path to the client-reconfig script
 #
-#   Default: undef
-# == Actions:
-#   - Install and configure Redis Sentinel
+# @example Basic inclusion
+#   include redis::sentinel
 #
-# == Sample Usage:
-#
-#   class { 'redis::sentinel': }
-#
+# @example Configuring options
 #   class {'redis::sentinel':
 #     down_after => 80000,
-#     log_file => '/var/log/redis/sentinel.log',
+#     log_file   => '/var/log/redis/sentinel.log',
 #   }
 #
 class redis::sentinel (
-  $auth_pass              = $redis::params::sentinel_auth_pass,
+  $auth_pass              = undef,
   $config_file            = $redis::params::sentinel_config_file,
   $config_file_orig       = $redis::params::sentinel_config_file_orig,
-  Stdlib::Filemode $config_file_mode = $redis::params::sentinel_config_file_mode,
-  $conf_template          = $redis::params::sentinel_conf_template,
+  Stdlib::Filemode $config_file_mode = '0644',
+  String[1] $conf_template = 'redis/redis-sentinel.conf.erb',
   $daemonize              = $redis::params::sentinel_daemonize,
-  $down_after             = $redis::params::sentinel_down_after,
-  $failover_timeout       = $redis::params::sentinel_failover_timeout,
+  $down_after             = 30000,
+  $failover_timeout       = 180000,
   $init_script            = $redis::params::sentinel_init_script,
-  $init_template          = $redis::params::sentinel_init_template,
+  String[1] $init_template = 'redis/redis-sentinel.init.erb',
   $log_level              = $redis::params::log_level,
   $log_file               = $redis::params::log_file,
-  $master_name            = $redis::params::sentinel_master_name,
-  Stdlib::Host $redis_host = $redis::params::sentinel_redis_host,
-  Stdlib::Port $redis_port = $redis::params::port,
+  String[1] $master_name  = 'mymaster',
+  Stdlib::Host $redis_host = '127.0.0.1',
+  Stdlib::Port $redis_port = 26379,
   $package_name           = $redis::params::sentinel_package_name,
-  $package_ensure         = $redis::params::sentinel_package_ensure,
-  $parallel_sync          = $redis::params::sentinel_parallel_sync,
-  $pid_file               = $redis::params::sentinel_pid_file,
-  $quorum                 = $redis::params::sentinel_quorum,
-  $sentinel_bind          = $redis::params::sentinel_bind,
+  String[1] $package_ensure = 'present',
+  Integer[0] $parallel_sync = 1,
+  Stdlib::Absolutepath $pid_file = '/var/run/redis/redis-sentinel.pid',
+  Integer[0] $quorum      = 2,
+  $sentinel_bind          = undef,
   Stdlib::Port $sentinel_port = $redis::params::sentinel_port,
   $service_group          = $redis::params::service_group,
-  $service_name           = $redis::params::sentinel_service_name,
+  String[1] $service_name = 'redis-sentinel',
   $service_ensure         = $redis::params::service_ensure,
   Boolean $service_enable = $redis::params::service_enable,
   $service_user           = $redis::params::service_user,
-  $working_dir            = $redis::params::sentinel_working_dir,
-  $notification_script    = $redis::params::sentinel_notification_script,
-  $client_reconfig_script = $redis::params::sentinel_client_reconfig_script,
+  Stdlib::Absolutepaht $working_dir = '/tmp',
+  $notification_script    = undef,
+  $client_reconfig_script = undef,
 ) inherits redis::params {
 
   require 'redis'

manifests/config.pp
@@ -1,7 +1,5 @@
-# = Class: redis::config
-#
-# This class provides configuration for Redis.
-#
+# @summary This class provides configuration for Redis.
+# @api private
 class redis::config {
 
   File {

manifests/install.pp
@@ -1,7 +1,5 @@
-# = Class: redis::install
-#
-# This class installs the application.
-#
+# @summary This class installs the application.
+# @api private
 class redis::install {
   if $redis::manage_package {
     package { $redis::package_name:

manifests/instance.pp
@@ -1,5 +1,3 @@
-# redis::instance
-#
 # This is an defined type to allow the configuration of
 # multiple redis instances on one machine without conflicts
 #

manifests/params.pp
@@ -1,7 +1,5 @@
-# = Class: redis::params
-#
-# This class provides a number of parameters.
-#
+# @summary This class provides a number of parameters.
+# @api private
 class redis::params {
   # Generic
   $manage_repo                = false
@@ -49,25 +47,6 @@ class redis::params {
   $requirepass                     = undef
   $save_db_to_disk                 = true
   $save_db_to_disk_interval        = {'900' =>'1', '300' => '10', '60' => '10000'}
-  $sentinel_auth_pass              = undef
-  $sentinel_bind                   = undef
-  $sentinel_config_file_mode       = '0644'
-  $sentinel_config_group           = 'root'
-  $sentinel_config_owner           = 'redis'
-  $sentinel_conf_template          = 'redis/redis-sentinel.conf.erb'
-  $sentinel_down_after             = 30000
-  $sentinel_failover_timeout       = 180000
-  $sentinel_master_name            = 'mymaster'
-  $sentinel_parallel_sync          = 1
-  $sentinel_port                   = 26379
-  $sentinel_quorum                 = 2
-  $sentinel_redis_host             = '127.0.0.1'
-  $sentinel_service_name           = 'redis-sentinel'
-  $sentinel_working_dir            = '/tmp'
-  $sentinel_init_template          = 'redis/redis-sentinel.init.erb'
-  $sentinel_pid_file               = '/var/run/redis/redis-sentinel.pid'
-  $sentinel_notification_script    = undef
-  $sentinel_client_reconfig_script = undef
   $service_provider                = undef
   $set_max_intset_entries          = 512
   $slave_priority                  = 100
@@ -125,7 +104,6 @@ class redis::params {
       $sentinel_daemonize        = true
       $sentinel_init_script      = '/etc/init.d/redis-sentinel'
       $sentinel_package_name     = 'redis-sentinel'
-      $sentinel_package_ensure   = 'present'
       $service_manage            = true
       $service_enable            = true
       $service_ensure            = 'running'
@@ -185,7 +163,6 @@ class redis::params {
       $sentinel_daemonize        = false
       $sentinel_init_script      = undef
       $sentinel_package_name     = 'redis'
-      $sentinel_package_ensure   = 'present'
       $service_manage            = true
       $service_enable            = true
       $service_ensure            = 'running'
@@ -240,7 +217,6 @@ class redis::params {
       $sentinel_daemonize        = true
       $sentinel_init_script      = undef
       $sentinel_package_name     = 'redis'
-      $sentinel_package_ensure   = 'present'
       $service_manage            = true
       $service_enable            = true
       $service_ensure            = 'running'
@@ -274,7 +250,6 @@ class redis::params {
       $sentinel_daemonize        = true
       $sentinel_init_script      = undef
       $sentinel_package_name     = 'redis'
-      $sentinel_package_ensure   = 'present'
       $service_manage            = true
       $service_enable            = true
       $service_ensure            = 'running'
@@ -309,7 +284,6 @@ class redis::params {
       $sentinel_daemonize        = true
       $sentinel_init_script      = undef
       $sentinel_package_name     = 'redis'
-      $sentinel_package_ensure   = 'present'
       $service_manage            = true
       $service_enable            = true
       $service_ensure            = 'running'

manifests/preinstall.pp
@@ -1,8 +1,6 @@
-# = Class: redis::preinstall
-#
-# This class provides anything required by the install class.
-# Such as package repositories.
-#
+# @summary Provides anything required by the install class, such as package
+#   repositories.
+# @api private
 class redis::preinstall {
   if $redis::manage_repo {
     case $::operatingsystem {

manifests/service.pp
@@ -1,7 +1,5 @@
-# = Class: redis::service
-#
-# This class manages the Redis daemon.
-#
+# @summary This class manages the Redis daemon.
+# @api private
 class redis::service {
   if $redis::service_manage {
     service { $redis::service_name:

commit 743a3b2e9776329b6bcb66a2d68a3abc4e5913ce

Fix regressions introduced in sentinel
228a5376f75044c7d98419f077e99486b2bb217a introduced regressions. This
fixes them.

manifests/sentinel.pp
@@ -119,20 +119,20 @@ class redis::sentinel (
   $log_file               = $redis::params::log_file,
   String[1] $master_name  = 'mymaster',
   Stdlib::Host $redis_host = '127.0.0.1',
-  Stdlib::Port $redis_port = 26379,
+  Stdlib::Port $redis_port = $redis::params::port,
   $package_name           = $redis::params::sentinel_package_name,
   String[1] $package_ensure = 'present',
   Integer[0] $parallel_sync = 1,
   Stdlib::Absolutepath $pid_file = '/var/run/redis/redis-sentinel.pid',
   Integer[0] $quorum      = 2,
   $sentinel_bind          = undef,
-  Stdlib::Port $sentinel_port = $redis::params::sentinel_port,
+  Stdlib::Port $sentinel_port = 26379,
   $service_group          = $redis::params::service_group,
   String[1] $service_name = 'redis-sentinel',
   $service_ensure         = $redis::params::service_ensure,
   Boolean $service_enable = $redis::params::service_enable,
   $service_user           = $redis::params::service_user,
-  Stdlib::Absolutepaht $working_dir = '/tmp',
+  Stdlib::Absolutepath $working_dir = '/tmp',
   $notification_script    = undef,
   $client_reconfig_script = undef,
 ) inherits redis::params {